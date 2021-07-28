c241b787c9350e73b386d6a9331d4890ea62c81c
Knose <os7138ce-s@student.lu.se>
2021-07-28 21:26:39 +0200

    Fix bug regarding uint value with int default

--- Options.cs
+++ Options.cs
@@ -3,7 +3,7 @@ using CommandLine;
 namespace tarot{
     [Verb("get", HelpText = "Retrieve cards from the deck.")]
             public class GetOptions{
-                [Value(0, MetaName = "amount", MetaValue = "int", Required = false, Default = 1, HelpText = "Amount of cards to retrieve.")]
+                [Value(0, MetaName = "amount", MetaValue = "uint", Required = false, Default = 1u, HelpText = "Amount of cards to retrieve.")]
                 public uint Amount{get; set;}
             }
 
@@ -11,8 +11,8 @@ namespace tarot{
     public class ShuffleOptions{
         [Value(0, MetaName = "type", MetaValue = "string", Required = false, Default = "riffle", HelpText = "What shuffle to perform.")]
         public string Type{get; set;}
-        [Value(1, MetaName = "amount", MetaValue = "int", Required = false, Default = 1, HelpText = "Number of shuffles to perform.")]
-        public int Amount{get; set;}
+        [Value(1, MetaName = "amount", MetaValue = "uint", Required = false, Default = 1u, HelpText = "Number of shuffles to perform.")]
+        public uint Amount{get; set;}
         [Option('q',"quiet", HelpText = "Suppress stdout.")]
         public bool Quiet{get; set;}
     }
